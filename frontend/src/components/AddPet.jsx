ec2acffd3d705f1901cc8ad365f386a756d06010
Ayush Talan <ayush.talan07@gmail.com>
2024-04-21 11:41:22 +0530

    added central login and improved card view

--- frontend/src/components/AddPet.jsx
+++ frontend/src/components/AddPet.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Sidebar3 from "./Sidebar3";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
