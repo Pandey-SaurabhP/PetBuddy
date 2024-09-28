816757f4ef3897a355926d4cb0625a08af51579b
Saurabh Pandey <40917276+Pandey-SaurabhP@users.noreply.github.com>
2024-09-29 03:20:42 +0530

    Latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,53 @@
-import Home from "./components/Home";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import PetHouses from "./components/PetHouses";
-import Dashboard from "./components/Dashboard";
-import Features from "./components/Features";
-import Profile from "./components/Profile";
-import TY from "./components/ty"
-import AddPetInfo from "./components/AddPet";
-import LoginForm from "./components/LoginPage";
-import SignupForm from "./components/SignupPage";
-import PetHouseProfile from "./components/PethouseProfile";
-import AdminProfile from "./PetHouses/Profile";
-import PetHousesLogin from "./PetHouses/Login";
-import PetHousesSignUp from "./PetHouses/SignUp";
-import AdminDashboard from "./PetHouses/Dashboard";
-import CentralLogin from "./components/CentralLogin";
-import VetClinics from "./components/VetClinics"
-import AdminLogin from "./components/AdminLogin";
-import AdminSignup from "./components/AdminSignup";
-function App() {
-  return (
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Dashboard/>}/>
-        <Route path="/home" element={<Dashboard/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/Pethouses" element={<PetHouses/>}/>
-        <Route path="/Features" element={<Features/>}/>
-        <Route path="/ty" element={<TY/>}/>
-        <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/signup" element={<SignupForm/>}/>
-        <Route path="/addpet" element={<AddPetInfo/>}/>
-        <Route path="/centrallogin" element={<CentralLogin/>}/>
-        <Route path="/AdminLogin" element={<PetHousesLogin/>}/>
-        <Route path="/AdminSignUp" element={<PetHousesSignUp/>}/>
-        <Route path="/AdminProfile" element={<AdminProfile />}/>
-        <Route path="/AdminDashboard" element={<AdminDashboard />}/>
-        <Route path="/petHouseProfile" element={<PetHouseProfile />}/>
-        <Route path="/vetClinics" element={<VetClinics />}/>
+    import Home from "./components/Home";
+    import {BrowserRouter, Routes, Route} from 'react-router-dom';
+    import PetHouses from "./components/PetHouses";
+    import Dashboard from "./components/Dashboard";
+    import Features from "./components/Features";
+    import Profile from "./components/Profile";
+    import TY from "./components/ty"
+    import AddPetInfo from "./components/AddPet";
+    import LoginForm from "./components/LoginPage";
+    import SignupForm from "./components/SignupPage";
+    import PetHouseProfile from "./components/PethouseProfile";
+    import AdminProfile from "./PetHouses/Profile";
+    import PetHousesLogin from "./PetHouses/Login";
+    import PetHousesSignUp from "./PetHouses/SignUp";
+    import AdminDashboard from "./PetHouses/Dashboard";
+    import CentralLogin from "./components/CentralLogin";
+    import VetClinics from "./components/VetClinics"
+    import AdminLogin from "./components/AdminLogin";
+    import AdminSignup from "./components/AdminSignup";
 
-        <Route path="/adminSignup" element={<AdminSignup />}/>
-        <Route path="/adminLogin" element={<AdminLogin />}/>
-      </Routes>
-      </BrowserRouter>
+    function App() {
+    return (
+        <>
+        <BrowserRouter>
+        <Routes>
+            <Route index element={<Dashboard/>}/>
+            <Route path="/home" element={<Dashboard/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/Pethouses" element={<PetHouses/>}/>
+            <Route path="/Features" element={<Features/>}/>
+            <Route path="/ty" element={<TY/>}/>
+            <Route path="/login" element={<LoginForm/>}/>
+            <Route path="/signup" element={<SignupForm/>}/>
+            <Route path="/addpet" element={<AddPetInfo/>}/>
+            <Route path="/centrallogin" element={<CentralLogin/>}/>
+            <Route path="/AdminLogin" element={<PetHousesLogin/>}/>
+            <Route path="/AdminSignUp" element={<PetHousesSignUp/>}/>
+            <Route path="/AdminProfile" element={<AdminProfile />}/>
+            <Route path="/AdminDashboard" element={<AdminDashboard />}/>
+            <Route path="/petHouseProfile" element={<PetHouseProfile />}/>
+            <Route path="/vetClinics" element={<VetClinics />}/>
 
-    </>
+            <Route path="/adminSignup" element={<AdminSignup />}/>
+            <Route path="/adminLogin" element={<AdminLogin />}/>
+        </Routes>
+        </BrowserRouter>
 
-  );
-}
+        </>
 
-export default App;
+    );
+    }
+
+    export default App;
